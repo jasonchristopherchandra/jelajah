2584989c0af91f0ceb101a0ac3f65a9f039db4fa
Jason CC <32058447+jasonchristopherchandra@users.noreply.github.com>
2019-04-03 21:58:23 +0700

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,109 @@
+<?php return array (
+  'backpack/base' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Base\\BaseServiceProvider',
+    ),
+  ),
+  'backpack/crud' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\CRUD\\CrudServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'CRUD' => 'Backpack\\CRUD\\CrudServiceProvider',
+    ),
+  ),
+  'backpack/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-elfinder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Elfinder\\ElfinderServiceProvider',
+    ),
+  ),
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'creativeorange/gravatar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Creativeorange\\Gravatar\\GravatarServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gravatar' => 'Creativeorange\\Gravatar\\Facades\\Gravatar',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laracasts/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laracasts\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'prologue/alerts' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Prologue\\Alerts\\AlertsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Alert' => 'Prologue\\Alerts\\Facades\\Alert',
+    ),
+  ),
+);
